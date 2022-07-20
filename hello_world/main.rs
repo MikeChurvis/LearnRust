b5730df32d7204366ca75febbbdd9a7862887149
Mike Churvis <mikechurvis@gmail.com>
2022-07-20 07:05:45 -0400

    Complete book module: Hello, World!

---
+++ hello_world/main.rs
@@ -0,0 +1,6 @@
+// The main function is the universal entrypoint into all executables built with Rust.
+fn main() {
+    // This is a macro, as are all invokable things with names that end in '!'.
+    // The distinction between macros and functions will be discussed in Chapter 19.
+    println!("Hello, world!");  
+}
