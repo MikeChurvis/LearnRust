751406e865ea441e06f7088fc09114f0d5803ce6
Mike Churvis <mikechurvis@gmail.com>
2022-07-20 22:36:37 -0400

    Update guessing_game

--- book/02_guessing_game/guessing_game/src/main.rs
+++ book/02_guessing_game/guessing_game/src/main.rs
@@ -1,3 +1,16 @@
+// use namespace::module
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    println!("Please input your guess.");
+
+    let mut guess = String::new(); // Type::associated_fn()
+
+    // module::function()
+    io::stdin()
+        .read_line(&mut guess) // Pass to .read_line() a mutable reference to 'guess'.
+        .expect("Failed to read line"); // If this Result is an Err, crash the program and print this message.
+
+    println!("You guessed: {guess}");
 }
