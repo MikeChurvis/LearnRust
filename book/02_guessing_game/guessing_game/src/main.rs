fccd780d740c8033ba217647bd3b7ace059a306e
Mike Churvis <mikechurvis@gmail.com>
2022-07-23 19:12:37 -0400

    Update guessing_game
    Adds looping. Dirty quit though; enter a non-number to crash the program.

--- book/02_guessing_game/guessing_game/src/main.rs
+++ book/02_guessing_game/guessing_game/src/main.rs
@@ -10,31 +10,33 @@ fn main() {
 
     println!("The secret number is: {secret_number}");
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    // Type::associated_fn()
-    let mut guess = String::new();
+        // Type::associated_fn()
+        let mut guess = String::new();
 
-    // Read standard input into the guess string.
-    io::stdin()
-        // Pass to .read_line() a mutable reference to 'guess'.
-        .read_line(&mut guess)
-        // If this Result is an Err, crash the program.
-        .expect("Failed to read line");
+        // Read standard input into the guess string.
+        io::stdin()
+            // Pass to .read_line() a mutable reference to 'guess'.
+            .read_line(&mut guess)
+            // If this Result is an Err, crash the program.
+            .expect("Failed to read line");
 
-    // Note: .expect() is far less useful than exception handling, taught in ch09.
+        // Note: .expect() is far less useful than exception handling, taught in ch09.
 
-    // Shadow the previous guess variable, this time with an immutable unsigned
-    // 32 bit number. Crash the program if the input is not parsable as a number.
-    // Note: .parse() relies on the type annotation to determine its output type.
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        // Shadow the previous guess variable, this time with an immutable unsigned
+        // 32 bit number. Crash the program if the input is not parsable as a number.
+        // Note: .parse() relies on the type annotation to determine its output type.
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-    println!("You guessed: {guess}");
+        println!("You guessed: {guess}");
 
-    // match thing { pattern => handler, ... }
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        // match thing { pattern => handler, ... }
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("You win!"),
+        }
     }
 }
