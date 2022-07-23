45dc532f4bc70d0b931b532fb069f2a10e83fc06
Mike Churvis <mikechurvis@gmail.com>
2022-07-23 19:09:31 -0400

    Update guessing_game
    Adds match statement to compare the user's input and the secret number. No looping yet.

--- book/02_guessing_game/guessing_game/src/main.rs
+++ book/02_guessing_game/guessing_game/src/main.rs
@@ -1,16 +1,40 @@
 // use namespace::module
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
+
     println!("Please input your guess.");
 
-    let mut guess = String::new(); // Type::associated_fn()
+    // Type::associated_fn()
+    let mut guess = String::new();
 
-    // module::function()
+    // Read standard input into the guess string.
     io::stdin()
-        .read_line(&mut guess) // Pass to .read_line() a mutable reference to 'guess'.
-        .expect("Failed to read line"); // If this Result is an Err, crash the program (this is far less useful than exception handling, taught in ch09).
+        // Pass to .read_line() a mutable reference to 'guess'.
+        .read_line(&mut guess)
+        // If this Result is an Err, crash the program.
+        .expect("Failed to read line");
+
+    // Note: .expect() is far less useful than exception handling, taught in ch09.
+
+    // Shadow the previous guess variable, this time with an immutable unsigned
+    // 32 bit number. Crash the program if the input is not parsable as a number.
+    // Note: .parse() relies on the type annotation to determine its output type.
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
     println!("You guessed: {guess}");
+
+    // match thing { pattern => handler, ... }
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
